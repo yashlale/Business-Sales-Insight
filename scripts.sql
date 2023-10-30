be0ca0c79bae735aec520c999761b03a14cdd9be
yashlale <144095593+yashlale@users.noreply.github.com>
2023-10-30 22:34:19 +0530

    Add files via upload

---
+++ scripts.sql
@@ -0,0 +1,10 @@
+use sales;
+SELECT * FROM customers;
+SELECT count(*) FROM customers;
+SELECT * FROM transactions where market_code='Mark001';
+SELECT distinct product_code FROM transactions where market_code='Mark001';
+SELECT * from transactions where currency="USD";
+SELECT transactions.*, date.* FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020;
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and transactions.currency="INR\r" or transactions.currency='USD\r';
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and date.month_name="January" and (transactions.currency="INR\r" or transactions.currency='USD\r');
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date=date.date where date.year=2020 and transactions.market_code="Mark001";
